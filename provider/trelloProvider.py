3a32dd308370262518a04142e14c54f67b8123ba
DmEremin <dm.eremin271090@gmail.com>
2021-06-06 17:51:39 +0300

    added implementation in provider

--- provider/trelloProvider.py
+++ provider/trelloProvider.py
@@ -1,6 +1,8 @@
 from trello import TrelloClient
+import config
 
 BOARD_ID = "Ql9mg3Lg"
+
 
 class TrelloProvider:
     def __init__(self, config):
@@ -14,9 +16,35 @@ class TrelloProvider:
 
     def get_tasks(self):
         board = self._client.get_board(self._board_id)
-        for card in board.get_cards():
-            print(card.name)
+        result = {'actions': {},
+                  'time_start': None,
+                  'time_end': None}
+        actions_set = []
+        pull_of_cards = board.get_cards()
 
-    @staticmethod
-    def uncheck_tasks():
-        pass
+        for card in pull_of_cards:
+            for el in card.fetch_checklists()[0].items:
+                actions_set.append(el['name'])
+        actions_set = set(actions_set)
+
+        for action in actions_set:
+            result['actions'][action] = {'plan': [], 'fact': []}
+
+        for card in pull_of_cards:
+            for el in card.fetch_checklists()[0].items:
+                result['actions'][el['name']]['plan'].append(card.name)
+                if el['state'] == 'complete':
+                    result['actions'][el['name']]['fact'].append(card.name)
+
+        return result
+
+    def uncheck_tasks(self):
+        board = self._client.get_board(self._board_id)
+        for card in board.get_cards():
+            for el in card.fetch_checklists()[0].items:
+                card.checklists[0].set_checklist_item(el['name'], False)
+
+
+trello_provider = TrelloProvider(config.TRELLO_CONFIG)
+trello_provider.get_tasks()
+trello_provider.uncheck_tasks()
--- storage/trelloTasksStorage.py
+++ storage/trelloTasksStorage.py
@@ -8,12 +8,10 @@ class TrelloTasksStorage:
         self.client = MongoClient(connection_string)
         self.db = self.client[db_name]
         self.tasks_db = self.db.tasks_db
-        self.collect_item_db = self.db.collect_item_db    
-    
-    
+        self.collect_item_db = self.db.collect_item_db
+
     def save_tasks(self, tasks):
         self.tasks_db.insert_one(tasks)
-    
-    
+
     def get_tasks(self):
         return [el for el in self.tasks_db.find()]
